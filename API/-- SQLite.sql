c3aa377ddd4bd1709cd0e0ffc39c53eb1bab5254
Lejla Basic <lejla.basic@edu.fit.ba>
2021-11-07 00:38:47 +0100

    EndSect2

---
+++ API/-- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, Username)
+VALUES (1,"samyfost");
+
+INSERT INTO Users (Id, Username)
+VALUES (2,"domky");
+
+INSERT INTO Users (Id, Username)
+VALUES (3,"dola");
+
+INSERT INTO Users (Id, Username)
+VALUES (4,"dzemba");
