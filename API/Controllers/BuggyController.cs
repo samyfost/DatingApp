546b19e1c0b564c300d08ed95d0d855a2611ad19
Lejla Basic <lejla.basic@edu.fit.ba>
2021-11-29 02:52:52 +0100

    summi

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -45,7 +45,7 @@ namespace API.Controllers
         [HttpGet("bad-request")]
         public ActionResult<string> GetBadRequest()
         {
-            return BadRequest("This was nt a good request");
+            return BadRequest();
         }
     }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -27,10 +27,12 @@ namespace API.Entities
 
    public ICollection<Photo> Photos { get; set; }
 
-//    public int GetAge() { 
-//        return DateOfBirth.CalculateAge();
-//     }
+    // public int GetAge() { 
+    //    return DateOfBirth.CalculateAge();
+    //  }
+public ICollection<UserLike> LikedByUsers { get; set; }
 
+public ICollection<UserLike> LikedUsers { get; set; }
 
     }
 }
